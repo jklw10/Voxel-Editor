d5abfd1d08a11e5d9ab3c320d2bfc020425050f0
jklw10 <112possu@gmail.com>
2021-02-04 19:13:45 +0200

    v0.1
    updated to use engine v0.1

--- Game.cs
+++ Game.cs
@@ -5,16 +5,21 @@ using Voxel_Engine;
 using OpenTK;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Graphics.OpenGL4;
+
 using System.Threading;
 using OpenTK.Windowing.Common;
+
+using Voxel_Engine.Utility;
+using Voxel_Engine.GUI;
+using Voxel_Engine.DataHandling;
+using Voxel_Engine.Rendering;
 
 namespace Voxel_Editor
 {
     class Game
     {
         static Camera cam;
-
-        
         public static void OnLoad()
         {
             
@@ -29,16 +34,19 @@ namespace Voxel_Editor
 
 
             cam = new Camera(Engine.window.Size);
-            cam.Select();
-            Engine.window.MouseMove += OnMouseMove;
             cam.LoadWorld(w);
+            cam.Select();
             UI.ImGuiAction += () => { };
             UI.IsActive = false;
+            Engine.window.RenderFrequency = 144;
         }
 
-        private static void OnMouseMove(MouseMoveEventArgs MMEArgs)
+        private static void RotateCamera(Vector2 dir)
         {
-            Camera.Main.CameraLookAt = (Quaternion.FromAxisAngle(Vector3.UnitX, MMEArgs.DeltaX) * Quaternion.FromAxisAngle(Vector3.UnitZ, MMEArgs.DeltaY)) * cam.CameraLookAt;
+            float speed = 0.02f;
+            Camera.Main.CameraLookAt =( Matrix4.CreateRotationZ(dir.X * speed) * Matrix4.CreateRotationY(dir.Y * speed) * new Vector4(Camera.Main.CameraLookAt)).Xyz;
+
+
         }
 
         public static void OnUpdate(FrameEventArgs obj)
@@ -46,11 +54,9 @@ namespace Voxel_Editor
             if (Input.KeyPress(Keys.Escape))
             {
                 Engine.window.CursorGrabbed = !Engine.window.CursorGrabbed;
-               // Engine.window.CursorVisible = !Engine.window.CursorVisible;
-                
             }
+            RotateCamera(Engine.window.MouseState.Delta);
         }
-        
         public static void OnFrame(FrameEventArgs obj)
         {
         }
